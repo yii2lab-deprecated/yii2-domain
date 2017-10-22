095c1c125752b911bcf34671daf1747ce4baaa2e
Vitaliy <theyamshikov@yandex.ru>
2017-10-23 02:05:31 +0600

    create update and delete buttons

--- src/views/active/view.php
+++ src/views/active/view.php
@@ -3,9 +3,25 @@
 /* @var $this yii\web\View
  * @var $entity yii2lab\domain\BaseEntity
  */
+use yii2lab\helpers\yii\Html;
 
 ?>
 
-<p>
+<div class="pull-right">
+	<?= Html::a(Html::fa('pencil', ['class' => 'text-primary']), ['/article/manage/update', 'id' => $entity->id], [
+		'class' => 'btn btn-default',
+		'title' => t('action', 'UPDATE'),
+	]) ?>
+	<?= Html::a(Html::fa('trash', ['class' => 'text-danger']), ['/article/manage/delete', 'id' => $entity->id], [
+		'class' => 'btn btn-default',
+		'title' => t('action', 'DELETE'),
+		'data' => [
+			'confirm' => t('yii', 'Are you sure you want to delete this item?'),
+			'method' => 'post',
+		],
+	]) ?>
+</div>
+
+<div>
 	<?= $entity->content ?>
-</p>
+</div>
