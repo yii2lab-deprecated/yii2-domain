1a84155b650fd0a32fff16d2a6961c3209892321
Vitaliy <theyamshikov@yandex.ru>
2018-01-13 21:38:16 +0600

    make methods in Driver enum

--- src/enums/Driver.php
+++ src/enums/Driver.php
@@ -24,8 +24,31 @@ class Driver extends BaseEnum {
 	const HEADER = 'header';
 	const COOKIE = 'cookie';
 	
+	public static function primary($withTest = false) {
+		$driver = env('domain.driver.primary');
+		if($driver == self::CORE) {
+			return $driver;
+		}
+		return self::test($driver, $withTest);
+	}
+	
+	public static function slave($withTest = false) {
+		$driver = env('domain.driver.slave');
+		return self::test($driver, $withTest);
+	}
+	
+	/**
+	 * @param bool $withTest
+	 *
+	 * @return array|mixed|null|string
+	 *
+	 * @deprecated moved to self::primary()
+	 */
 	public static function remote($withTest = false) {
-		$driver = env('remote.driver');
+		$driver = env('domain.driver.primary');
+		if(empty($driver)) {
+			$driver = env('remote.driver');
+		}
 		if($driver == self::CORE) {
 			return $driver;
 		}
