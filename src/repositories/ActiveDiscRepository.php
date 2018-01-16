04391fec9714a447956d502fae8a98f889b7952e
Vitaliy <theyamshikov@yandex.ru>
2018-01-16 19:53:50 +0600

    clean code

--- src/repositories/ActiveDiscRepository.php
+++ src/repositories/ActiveDiscRepository.php
@@ -6,12 +6,10 @@ use yii2lab\domain\interfaces\repositories\ModifyInterface;
 use yii2lab\domain\interfaces\repositories\ReadInterface;
 use yii2lab\domain\traits\ArrayModifyTrait;
 use yii2lab\domain\traits\ArrayReadTrait;
-use yii2lab\domain\traits\RelationTrait;
 
 class ActiveDiscRepository extends DiscRepository2 implements ReadInterface, ModifyInterface {
 
 	use ArrayReadTrait;
 	use ArrayModifyTrait;
-	//use RelationTrait;
 	
 }
