d6401d7bff9e5feec4385a639c16d6314e3565ce
Vitaliy <theyamshikov@yandex.ru>
2018-01-19 00:57:47 +0600

    update test

--- tests/unit/services/ArticleTest.php
+++ tests/unit/services/ArticleTest.php
@@ -7,6 +7,9 @@ use common\fixtures\ArticleCategoryFixture;
 use common\fixtures\ArticleFixture;
 use Yii;
 use yii2lab\domain\BaseEntity;
+use yii2lab\domain\data\Query;
+use yii2module\article\domain\entities\ArticleEntity;
+use yii2module\article\domain\entities\CategoryEntity;
 
 class ArticleTest extends Unit
 {
@@ -32,10 +35,18 @@ class ArticleTest extends Unit
 	public function testFetch()
 	{
 		/** @var BaseEntity $entity */
-		$entity = Yii::$app->article->article->oneById(1);
-		expect(1)->equals($entity->id);
-		expect('about')->equals($entity->name);
-		expect('О нас')->equals($entity->title);
+		$query = Query::forge();
+		$query->with('categories');
+		$entity = Yii::$app->article->article->all($query);
+		
+		expect(true)->equals($entity[0] instanceof ArticleEntity);
+		expect(1)->equals($entity[0]->id);
+		expect('about')->equals($entity[0]->name);
+		expect(true)->equals($entity[0]->categories[0] instanceof CategoryEntity);
+		expect(1)->equals($entity[0]->categories[0]->id);
+		
+		expect(1)->equals($entity[2]->categories[0]->id);
+		expect(2)->equals($entity[2]->categories[1]->id);
 	}
 	
 }
