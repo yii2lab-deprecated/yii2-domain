01888e93b1acd9a1b4f7741ba82ee82651f1cc19
Vitaliy <theyamshikov@yandex.ru>
2018-01-21 01:21:29 +0600

    refacto

--- src/filters/LoadDomainConfig.php
+++ src/filters/LoadDomainConfig.php
@@ -2,57 +2,16 @@
 
 namespace yii2lab\domain\filters;
 
-use Yii;
 use yii2lab\app\domain\filters\config\LoadConfig;
 use yii2lab\designPattern\filter\interfaces\FilterInterface;
-use yii2lab\helpers\Helper;
-use yii2mod\helpers\ArrayHelper;
+use yii2lab\domain\helpers\ConfigHelper;
 
 class LoadDomainConfig extends LoadConfig implements FilterInterface {
 	
 	public $assignTo = 'components';
 	
 	protected function normalizeItem($domainId, $data) {
-		$data = Helper::normalizeComponentConfig($data);
-		$data = Helper::isEnabledComponent($data);
-		if(empty($data)) {
-			return null;
-		}
-		$data = $this->normalizeSubItems($domainId, $data);
-		if(!is_numeric($domainId)) {
-			$data['id'] = $domainId;
-		}
-		if(!empty($data['class'])) {
-			$domainInstance = Yii::createObject($data['class']);
-			$domainConfig = $domainInstance->config();
-			if(!empty($domainConfig)) {
-				$domainConfig = $this->normalizeSubItems($domainId, $domainConfig);
-				$data = ArrayHelper::merge($data, $domainConfig);
-			}
-		}
-		return $data;
-	}
-	
-	protected function normalizeSubItems($domainId, $config) {
-		if(!empty($config['services'])) {
-			$config['services'] = $this->genConfigs($config['services']);
-		}
-		if(!empty($config['repositories'])) {
-			$config['repositories'] = $this->genConfigs($config['repositories']);
-		}
-		return $config;
-	}
-	
-	protected function genConfigs($components) {
-		$configNew = [];
-		foreach($components as $id => $config) {
-			if(is_integer($id)) {
-				$id = $config;
-				$config = [];
-			}
-			$configNew[$id] = $config;
-		}
-		return $configNew;
+		return ConfigHelper::normalizeItemConfig($domainId, $data);
 	}
 	
 }
---
+++ src/helpers/ConfigHelper.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace yii2lab\domain\helpers;
+
+use Yii;
+use yii\helpers\ArrayHelper;
+use yii2lab\helpers\Helper;
+
+class ConfigHelper {
+	
+	public static function normalizeItemConfig($domainId, $data) {
+		$data = Helper::normalizeComponentConfig($data);
+		$data = Helper::isEnabledComponent($data);
+		if(empty($data)) {
+			return null;
+		}
+		$data = self::normalizeSubItems($domainId, $data);
+		if(!is_numeric($domainId)) {
+			$data['id'] = $domainId;
+		}
+		if(!empty($data['class'])) {
+			$domainInstance = Yii::createObject($data['class']);
+			$domainConfig = $domainInstance->config();
+			if(!empty($domainConfig)) {
+				$domainConfig = self::normalizeSubItems($domainId, $domainConfig);
+				$data = ArrayHelper::merge($data, $domainConfig);
+			}
+		}
+		return $data;
+	}
+	
+	protected static function normalizeSubItems($domainId, $config) {
+		if(!empty($config['services'])) {
+			$config['services'] = self::genConfigs($config['services']);
+		}
+		if(!empty($config['repositories'])) {
+			$config['repositories'] = self::genConfigs($config['repositories']);
+		}
+		return $config;
+	}
+	
+	protected static function genConfigs($components) {
+		$configNew = [];
+		foreach($components as $id => $config) {
+			if(is_integer($id)) {
+				$id = $config;
+				$config = [];
+			}
+			$configNew[$id] = $config;
+		}
+		return $configNew;
+	}
+	
+}
