d387fc3e955bad042192dc34f7025d02f676e9ed
Vitaliy <theyamshikov@yandex.ru>
2018-01-21 20:23:43 +0600

    update test

--- tests/_config/unit.php
+++ tests/_config/unit.php
@@ -6,7 +6,7 @@ use yii2lab\app\domain\helpers\Config;
 use yii2lab\app\domain\helpers\Env;
 use yii2lab\helpers\yii\ArrayHelper;
 
-Env::init('vendor/yii2lab/yii2-app/tests/store/app/common/config');
+Env::init('vendor/yii2lab/yii2-app/tests/store/app');
 $definition = Env::get('config');
 $config = @include('main.php');
 return ArrayHelper::merge(Config::load($definition), $config);
