86588f10152af00fb4fb623285e059a60e4fdf6e
Vitaliy <theyamshikov@yandex.ru>
2018-01-25 00:45:20 +0600

    update test

--- tests/_config/domains.php
+++ tests/_config/domains.php
@@ -1,6 +1,6 @@
 <?php
 
-$config = require(ROOT_DIR . DS . TEST_APPLICATION_DIR . '//common/config/domains.php');
+$config = require(ROOT_DIR . DS . TEST_APPLICATION_DIR . '/common/config/domains.php');
 
 return \yii\helpers\ArrayHelper::merge($config, [
 	
