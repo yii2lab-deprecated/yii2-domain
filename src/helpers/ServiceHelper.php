761e67f4f7a81daf6ede5669edb6147dc077a452
Vitaliy <theyamshikov@yandex.ru>
2018-03-04 22:22:19 +0600

    refacto

--- src/helpers/ServiceHelper.php
+++ src/helpers/ServiceHelper.php
@@ -4,18 +4,22 @@ namespace yii2lab\domain\helpers;
 
 use Yii;
 use yii\base\InvalidConfigException;
-use yii\helpers\ArrayHelper;
 use yii\web\NotFoundHttpException;
+use yii2mod\helpers\ArrayHelper;
 
 class ServiceHelper {
 	
 	public static function has($serviceName) {
-		list($domain, $service) = explode('.', $serviceName);
-		$services = config('components.' . $domain . '.services', []);
-		if(!isset($services[$service]) && !in_array($service, $services)) {
-			return false;
+		$serviceInstance = ArrayHelper::getValue(Yii::$app, $serviceName);
+		return is_object($serviceInstance);
+	}
+	
+	public static function get($serviceName) {
+		$serviceInstance = ArrayHelper::getValue(Yii::$app, $serviceName);
+		if(!is_object($serviceInstance)) {
+			throw new InvalidConfigException("Service \"$serviceName\" not found");
 		}
-		return true;
+		return $serviceInstance;
 	}
 	
 	public static function oneById($serviceName, $id)
@@ -23,10 +27,7 @@ class ServiceHelper {
 		if(!self::has($serviceName)) {
 			return null;
 		}
-		$serviceInstance = ArrayHelper::getValue(Yii::$app, $serviceName);
-		if(!is_object($serviceInstance)) {
-			throw new InvalidConfigException("Service \"$serviceName\" not found");
-		}
+		$serviceInstance = self::get($serviceName);
 		try {
 			$result = $serviceInstance->oneById($id);
 		} catch (NotFoundHttpException $e) {
