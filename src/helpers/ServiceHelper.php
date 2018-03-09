7017f7ad3ac5d8acb4243373953e7f6f5f4dcb49
Vitaliy <theyamshikov@yandex.ru>
2018-03-09 23:04:53 +0600

    update helper

--- src/helpers/ServiceHelper.php
+++ src/helpers/ServiceHelper.php
@@ -5,6 +5,8 @@ namespace yii2lab\domain\helpers;
 use Yii;
 use yii\base\InvalidConfigException;
 use yii\web\NotFoundHttpException;
+use yii2lab\domain\BaseEntity;
+use yii2lab\domain\services\BaseService;
 use yii2mod\helpers\ArrayHelper;
 
 class ServiceHelper {
@@ -14,14 +16,31 @@ class ServiceHelper {
 		return is_object($serviceInstance);
 	}
 	
-	public static function get($serviceName) {
-		$serviceInstance = ArrayHelper::getValue(Yii::$app, $serviceName);
+	/**
+	 * @param      $service
+	 * @param mixed $default
+	 *
+	 * @return BaseService
+	 * @throws InvalidConfigException
+	 */
+	public static function get($service, $default = null) {
+		if($service instanceof BaseService) {
+			return $service;
+		}
+		$serviceInstance = ArrayHelper::getValue(Yii::$app, $service, $default);
 		if(!is_object($serviceInstance)) {
-			throw new InvalidConfigException("Service \"$serviceName\" not found");
+			throw new InvalidConfigException("Service \"$service\" not found");
 		}
 		return $serviceInstance;
 	}
 	
+	/**
+	 * @param $serviceName
+	 * @param $id
+	 *
+	 * @return BaseEntity
+	 * @throws InvalidConfigException
+	 */
 	public static function oneById($serviceName, $id)
 	{
 		if(!self::has($serviceName)) {
