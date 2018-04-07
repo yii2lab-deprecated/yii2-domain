8f8b121805e6f1ae2d4e3bbf7274191041d91b9e
Vitaliy <theyamshikov@yandex.ru>
2018-04-08 02:30:40 +0600

    handle vendor error

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -6,6 +6,10 @@ $name = 'console';
 $path = '../../../..';
 defined('YII_ENV') OR define('YII_ENV', 'test');
 
-require_once(__DIR__ . '/' . $path . '/vendor/yii2lab/yii2-app/src/App.php');
+@include_once(__DIR__ . '/' . $path . '/vendor/yii2lab/yii2-app/src/App.php');
+
+if(!class_exists('yii2lab\app\App')) {
+	die('Run composer install');
+}
 
 App::init($name, __DIR__ . '/_application');
