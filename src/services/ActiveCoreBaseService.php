a7f914fcb6b00b1bbc90164a2020d7d055317402
Vitaliy <theyamshikov@yandex.ru>
2018-04-18 22:22:11 +0600

    fix core

--- src/services/ActiveCoreBaseService.php
+++ src/services/ActiveCoreBaseService.php
@@ -2,21 +2,27 @@
 
 namespace yii2lab\domain\services;
 
-use yii2lab\core\domain\repositories\base\BaseCoreRepository;
+use yii2lab\core\domain\repositories\base\BaseActiveCoreRepository;
 use yii2lab\helpers\ClassHelper;
 
-class CoreBaseService extends BaseService {
+class ActiveCoreBaseService extends ActiveBaseService {
 	
 	public $point = EMP;
 	/**
-	 * @var BaseCoreRepository
+	 * @var BaseActiveCoreRepository
 	 */
 	private $coreRepository;
 	
+	/**
+	 * @param null $name
+	 *
+	 * @return mixed
+	 */
 	public function getRepository($name = null) {
+		
 		if(!isset($this->coreRepository)) {
 			$this->coreRepository = ClassHelper::createObject([
-				'class' => BaseCoreRepository::class,
+				'class' => BaseActiveCoreRepository::class,
 				'domain' => $this->domain,
 				'id' => $this->id,
 				'point' => $this->point,
