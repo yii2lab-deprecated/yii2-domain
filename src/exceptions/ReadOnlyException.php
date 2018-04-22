c8797a824f3b15d01e13786a0b51df53ecd02b38
Vitaliy <theyamshikov@yandex.ru>
2018-04-22 21:54:27 +0600

    make classes

---
+++ src/exceptions/ReadOnlyException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace yii2lab\domain\exceptions;
+
+use yii\base\Exception;
+
+class ReadOnlyException extends Exception {
+
+}
---
+++ src/values/UserIdValue.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace yii2lab\domain\values;
+
+use Yii;
+use yii2lab\domain\exceptions\ReadOnlyException;
+
+class UserIdValue extends BaseValue {
+	
+	protected function _encode($value) {
+		//return intval($value);
+	}
+	
+	public function set($value) {
+		throw new ReadOnlyException();
+	}
+	
+	public function get($default = null) {
+		return Yii::$domain->account->auth->identity->id;
+	}
+	
+	public function isValid($value) {
+		return false;
+	}
+	
+}
