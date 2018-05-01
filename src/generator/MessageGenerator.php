8f4dd96bfbd60253584900037e252cf62e6ebaad
Vitaliy <theyamshikov@yandex.ru>
2018-05-01 19:12:32 +0600

    upgrade

--- src/generator/MessageGenerator.php
+++ src/generator/MessageGenerator.php
@@ -3,14 +3,14 @@
 namespace yii2lab\domain\generator;
 
 use yii\helpers\Inflector;
-use yii2lab\extension\code\helpers\ClassHelper;
+use yii2lab\extension\code\helpers\CodeHelper;
 
 class MessageGenerator extends BaseGenerator {
 
 	public $name;
 	
 	public function run() {
-		ClassHelper::generatePhpData($this->name, [
+		CodeHelper::generatePhpData($this->name, [
 			'title' => Inflector::humanize(basename($this->name)),
 		]);
 	}
