286361132e58c96a4f989d0361051de07333629f
Vitaliy <theyamshikov@yandex.ru>
2018-05-03 23:58:54 +0600

    make query filter

---
+++ src/helpers/repository/QueryFilter.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace yii2lab\domain\helpers\repository;
+
+use yii\base\BaseObject;
+use yii2lab\domain\data\Query;
+use yii2lab\domain\repositories\BaseRepository;
+
+/**
+ * Class QueryFilter
+ *
+ * @package yii2lab\domain\helpers\repository
+ *
+ * @property Query $query
+ */
+class QueryFilter extends BaseObject {
+	
+	/**
+	 * @var BaseRepository
+	 */
+	public $repository;
+	private $query;
+	private $with;
+	
+	public function getQueryWithoutRelations() {
+		$query = clone $this->query;
+		$this->with = RelationWithHelper::cleanWith($this->repository->relations(), $query);
+		return $query;
+	}
+	
+	public function loadRelations($data) {
+		if(empty($this->with)) {
+			return $data;
+		}
+		return RelationHelper::load($this->repository->domain->id, $this->repository->id, $this->query, $data);
+	}
+	
+	public function getQuery() {
+		return $this->query;
+	}
+	
+	public function setQuery($query) {
+		$this->query = clone $query;
+	}
+	
+}
