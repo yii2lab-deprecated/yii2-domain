8ec755022172855bd0113ae59f4992d77e42f80b
Vitaliy <theyamshikov@yandex.ru>
2018-06-26 10:45:06 +0600

    refacto

---
+++ src/traits/repository/CacheTrait.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace yii2lab\domain\traits\repository;
+
+use Closure;
+use Yii;
+use yii\helpers\ArrayHelper;
+
+trait CacheTrait {
+	
+	protected $cacheComponentName = 'cache';
+	
+	protected function cacheMethod($method, $params, $duration = null, Closure $isValidClosure = null) {
+		$cacheKey = static::class . DOT . $method . DOT . serialize($params);
+		$cacheComponent = $this->getCacheComponent();
+		$data = $cacheComponent->get($cacheKey);
+		if(empty($data)) {
+			$data = call_user_func_array('parent::' . $method, $params);
+			$isValidData = $this->isValidDataByClosure($data, $isValidClosure);
+			if($isValidData) {
+				$cacheComponent->set($cacheKey, $data, $duration);
+			} else {
+				$cacheComponent->delete($cacheKey);
+			}
+		}
+		return $data;
+	}
+	
+	private function getCacheComponent($cacheComponentName = null) {
+		$cacheComponentName = $cacheComponentName ?: $this->cacheComponentName;
+		/** @var \yii\caching\CacheInterface $component */
+		$component = ArrayHelper::getValue(Yii::$app, $cacheComponentName);
+		return $component;
+	}
+	
+	private function isValidDataByClosure($data, Closure $isValidClosure = null) {
+		if($isValidClosure === null) {
+			return true;
+		}
+		return (bool) $isValidClosure($data);
+	}
+	
+}
