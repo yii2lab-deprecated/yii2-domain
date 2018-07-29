7f0662d4c438c26181ded7255aa863da514926dd
Vitaliy <theyamshikov@yandex.ru>
2018-07-30 01:03:25 +0600

    fix

--- src/values/LangValue.php
+++ src/values/LangValue.php
@@ -7,14 +7,14 @@ use yii2module\lang\domain\helpers\LangHelper;
 class LangValue extends BaseValue {
 	
 	protected function _encode($value) {
-		if(empty($value)) {
+		if(is_null($value)) {
 			return '';
 		}
 		return LangHelper::extract($value);
 	}
 	
 	public function isValid($value) {
-		if(empty($value)) {
+		if(is_null($value)) {
 			return true;
 		}
 		return is_string($value) || is_array($value);
