806b3aaa2bf10de092a648a5cee6104d90a02cdc
Vitaliy <theyamshikov@yandex.ru>
2018-10-01 22:51:47 +0600

    make search

--- src/web/actions/IndexAction.php
+++ src/web/actions/IndexAction.php
@@ -3,20 +3,43 @@
 namespace yii2lab\domain\web\actions;
 
 use Yii;
+use yii\base\Model;
 use yii2lab\domain\base\Action;
 use yii2lab\domain\data\ActiveDataProvider;
-use yii2lab\domain\data\Query;
 
 class IndexAction extends Action {
 	
 	public $serviceMethod = 'getDataProvider';
+	public $searchClass = null;
 	
 	public function run() {
 		$this->view->title = Yii::t('main', 'list_title');
+		$searchModel = $this->loadSearchModel();
 		$method = $this->serviceMethod;
 		/** @var ActiveDataProvider $dataProvider */
 		$dataProvider = $this->service->$method($this->query);
 		//$dataProvider->query = Query::forge($this->query);
-		return $this->render($this->render, compact('dataProvider'));
+		return $this->render($this->render, compact('dataProvider', 'searchModel'));
 	}
+	
+	private function loadSearchModel() {
+		$searchModel = null;
+		if ($this->searchClass) {
+			/** @var Model $searchModel */
+			$searchModel = new $this->searchClass;
+			$formName = $searchModel->formName();
+			$params = Yii::$app->getRequest()->getQueryParams();
+			if(!empty($params[$formName])) {
+				$this->query->removeWhere($formName);
+				foreach($params[$formName] as $name => $value) {
+					if($value !== '') {
+						$this->query->andWhere([$name => $value]);
+					}
+				}
+				$searchModel->load($params);
+			}
+		}
+		return $searchModel;
+	}
+	
 }
