e90e384f4b9ef346e1f34cf1864e373f1342fcc9
amubarak@wooppay.kz <amubarak@wooppay.kz>
2019-11-13 10:43:14 +0600

    fix test

--- tests/_application/common/config/main.php
+++ tests/_application/common/config/main.php
@@ -5,7 +5,7 @@ use yii2lab\test\helpers\TestHelper;
 use yii2module\lang\domain\enums\LanguageEnum;
 
 $config = [
-	'language' => LanguageEnum::EN, // current Language
+
 ];
 
 $baseConfig = TestHelper::loadConfig('common/config/main.php');
